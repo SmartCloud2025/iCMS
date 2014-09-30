cf589de271aa9fd74060e111597c68c117057247
coolmoo <idreamsoft@qq.com>
2014-09-30 18:54:46 +0800

    0930

---
+++ public/ui/iCMS.login-6.0.0.js
@@ -0,0 +1,60 @@
+(function($) {
+  iCMS.user.login = function(boxid,success) {
+    var login = $(boxid);
+    login.on("click", '.iCMS_login_btn',function(){
+      var param = {'action': 'login'};
+      param.uname = $(".iCMS_login_uname", login).val();
+      if (login_check('uname', param.uname)){
+        return false
+      };
+
+      param.pass = $(".iCMS_login_pass", login).val();
+      if (login_check('pass', param.pass)){
+        return false
+      };
+
+      var seccode = $(".iCMS_login_seccode", login);
+      if(seccode.length){
+        param.seccode = seccode.val();
+        if (login_check('seccode', param.seccode)){
+          return false
+        };
+      }
+
+      param.remember = $(".iCMS_login_remember:checked", login).val();
+
+      $.post(iCMS.api('user'), param, function(ret) {
+        if (ret.code) {
+          window.location.href = ret.forward;
+        } else {
+          iCMS.alert(ret.msg);
+        }
+      }, 'json');
+    })
+
+    function login_check(el, val, def) {
+      def = def || '';
+      var info = {
+        'uname':'请输入登录名',
+        'pass':'请输入密码',
+        'seccode':'请输入验证码',
+      }
+      var b = $(".iCMS_login_" + el,login);
+      var container = b.attr("data-container");
+      if(container){
+        $(container).empty();
+      }
+      b.tooltip('destroy');
+      if (val == def) {
+        b.focus();
+        iCMS.tip(b, '<i class="fa fa-minus-circle"></i> '+info[el]);
+        return true;
+      } else {
+        if (typeof(success) === "function") {
+          success(b);
+        }
+        return false;
+      }
+    }
+  }
+})(jQuery);
